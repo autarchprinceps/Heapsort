5aed1745ebb05f037889a7deb164e1f24f384755
autarchprinceps <autarch@live.de>
2012-06-28 10:59:22 +0200

    Commandlist

---
+++ Patrick/Heapsort/src/heapsort/Instruction.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package heapsort;
+
+
+/**
+ *
+ * @author autarch
+ */
+public enum Instruction {
+    ComparisonParent, ComparisonFlat, Swap, Finish
+}
