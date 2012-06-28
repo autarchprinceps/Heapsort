85877d2c5b3d5a5bcd014e67a768fe233e141835
autarchprinceps <autarch@live.de>
2012-06-28 11:02:04 +0200

    Command

---
+++ Patrick/Heapsort/src/heapsort/Command.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package heapsort;
+
+/**
+ *
+ * @author autarch
+ */
+public class Command {
+    public int[] State;
+    public int FirstIndex;
+    public int SecondIndex;
+    Instruction Type;
+    
+    public Command(int [] state, int first, int second, Instruction type) {
+        State = state.clone();
+        FirstIndex = first;
+        SecondIndex = second;
+        Type = type;
+    }
+    
+    @Override
+    public String toString() {
+        return Heapsort.toString(State) + "\n" + FirstIndex + SecondIndex + Type;
+    }
+}
